cd9b33d53a3f513051764d199c8a8f4c9c6b7758
charanraj <charanrajjb007@gmail.com>
2023-09-26 21:30:43 +0530

    Programming

---
+++ src/com/pattern/RightPascalTriangle.java
@@ -0,0 +1,26 @@
+package com.pattern;
+
+public class RightPascalTriangle {
+	public static void main(String[] args) {
+		int row = 5;
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print(" * ");
+			}
+			System.out.println(" ");
+		}
+        row= 4;
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < row; j++) {
+				if (j >= (row - i)) {
+					System.out.print(" ");
+				} else {
+					System.out.print(" * ");
+				}
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
