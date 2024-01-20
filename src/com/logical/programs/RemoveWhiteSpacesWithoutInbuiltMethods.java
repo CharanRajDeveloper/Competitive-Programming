d127b57dec9e8f403c94cee198f74bcede7da2a0
Charan Raj MS <55379472+CharanRajDeveloper@users.noreply.github.com>
2024-01-20 11:13:34 +0530

    Update RemoveWhiteSpacesWithoutInbuiltMethods.java

--- src/com/logical/programs/RemoveWhiteSpacesWithoutInbuiltMethods.java
+++ src/com/logical/programs/RemoveWhiteSpacesWithoutInbuiltMethods.java
@@ -2,7 +2,7 @@ package com.logical.programs;
 
 public class RemoveWhiteSpacesWithoutInbuiltMethods {
 	public static void main(String[] args) {
-		String str="Charan is a Good Boy";
+		String str="Charan is a Java Developer";
 		char[] ch=str.toCharArray();
 		StringBuilder sb=new StringBuilder();
 		for(int i=0;i<ch.length;i++) {
